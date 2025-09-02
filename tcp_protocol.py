e13aeb183667361d8ff60ff0df28b6c8b541a551
Alexander-Adjei <akadjei017@st.ug.edu.gh>
2025-09-02 21:29:58 +0000

    SERVER_SIDE_UPDATE: Gracefull exit, Thread update, handle_conn & broadcaster modefied

--- tcp_protocol.py
+++ tcp_protocol.py
@@ -8,65 +8,77 @@ class TCP_Logic:
         clients = []
         clients_lock = threading.Lock()
         # Create a socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, backlog=None, reuse_port=False)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         print("Socket created successfully..")
         # Bind the socket
         s.bind((HOST, PORT))
         # Start listerning
         s.listen(6)
         print("Server is listerning...")
-
-        # Accept Connection request from client
-        while True:
-            conn, addr = s.accept()
-            threading.Thread(target=handle_conn, args=(conn, addr, clients), daemon=True)
         
-            def  handle_conn (conn, addr, clients):
+        def  handle_conn (conn, addr, clients, clients_lock, msgs):
                 with clients_lock:
                     clients.append(conn)
                 print(f"conneted to {addr}")
                 try:
                     while True:
-                        
                         # Read Msg form client
-                        message = conn.recv(2040)
+                        message = conn.recv(1024)
                         if not message:
-                            with clients_lock:
-                                clients.remove(conn)
                             break
                         msgs.put((conn, message))
                 finally:
                     with clients_lock:
                         clients.remove(conn)
-                        conn.close()
-            
-            handle_conn(conn, addr, clients)
+                    conn.close()
+                    print(f"Connection closed: {addr}")
 
-            #  Broacast msgs
-            def broadcaster(msgs, clients):
+        #  Broadcast msgs
+        def broadcaster(msgs, clients, clients_lock):
                 while True:
-                    threading.Thread(target=broadcaster, args=(msgs, clients), daemon=True)
                     sender, msg = msgs.get()
                     with clients_lock:
                         for c in clients:
                             if c != sender:
                                 c.sendall(msg)
 
-            broadcaster(msgs, clients)
+        # start broadcasting
+        threading.Thread(
+            target=broadcaster, 
+            args=(msgs, clients, clients_lock),
+            daemon=True
+            ).start()
 
-    
-    def tcp_Protocol_clientSide():
-        HOST = "127.0.0.1"
-        PORT = 40252
-        msg = "Hello server, this is client"
-        # Create a socket
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # Establish connection to server
-        s.connect((HOST, PORT))
-        # Send Msg to server
-        s.send(2024)
-        # Recieve Reply from server
-        s.recv(2040)
+        #  Accept Connection request from client (loop)
+        try:
+            while True:
+                conn, addr = s.accept()
+                # Start handling connections
+                threading.Thread(
+                    target=handle_conn,
+                    args=(conn, addr, clients, clients_lock, msgs),
+                    daemon=True
+                    ).start()
+        except KeyboardInterrupt as ki:
+            print("\n Shutting down Server...")
+            with clients_lock:
+                for c in clients:
+                    c.close()
+                s.close()
+                sys.exit(0)  #clean exit
+
+    # def tcp_Protocol_clientSide():
+    #     HOST = "127.0.0.1"
+    #     PORT = 40252
+    #     msg = "Hello server, this is client"
+    #     # Create a socket
+    #     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #     # Establish connection to server
+    #     s.connect((HOST, PORT))
+    #     # Send Msg to server
+    #     s.send(2024)
+    #     # Recieve Reply from server
+    #     s.recv(2040)
     
 
 TCP_Logic.tcp_protocol_serverSide()
